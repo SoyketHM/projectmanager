da715eb985c4025e69cb90b4c255a5691d324c30
Soyket <soykethandymama@gmail.com>
2018-08-07 16:35:31 +0600

    add from

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
 import React, { Component } from 'react';
 import Projects from './Components/Projects';
+import uuid from 'uuid';
+import AddProject from './Components/AddProject';
 import './App.css';
 
 class App extends Component {
     constructor(){
         super();
         this.state = {
+            projects: []
+        }
+    }
+
+    componentWillMount(){
+        this.setState( {
             projects: [
                 {
+                    id: uuid.v4(),
                     title: "Business Website",
                     category: "Web Design"
                 },
                 {
+                    id: uuid.v4(),
                     title: "Social App",
                     category: "Mobile Development"
                 },
                 {
+                    id: uuid.v4(),
                     title: "Ecommarce Shoping Cart",
                     category: "Web Development"
                 }
             ]
-        }
+        });
     }
+
+    handleAddProject(project){
+        let projects = this.state.projects;
+        projects.push(project);
+        this.setState( {projects: projects} );
+    }
+
     render() {
         return (
             <div className="App">
-                My App
-                <Projects project={this.state.projects} />
+                <AddProject addProject={this.handleAddProject.bind(this)}/>
+                <Projects projects={this.state.projects} />
             </div>
         );
     }
